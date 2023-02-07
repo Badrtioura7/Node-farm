9c85461ec912a454302c1f1025155927e1014e7e
Badrtioura7 <mohamedtioura7@gmail.com>
2023-02-07 18:15:59 +0100

    combining files together

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
-const textIn =  fs.readFileSync('./txt/input.txt', 'utf-8');
-const textOut = `this is : ${textIn}, Created on, ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('file has been written!');
+// const textIn =  fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textOut = `this is : ${textIn}, Created on, ${Da te.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('file has been written!');
+fs.readFile('./txt/start.txt' ,'utf-8',  (err, data1 )=> {
+   fs.readFile(`./txt/${data1}.txt`, 'utf-8',(err,data2 )=>{
+     console.log(data2);
+      fs.readFile('./txt/append.txt', 'utf-8',(err, data3)=>{
+      console.log(data3);
+        fs.writeFile('./txt/final.txt', `${data2} \n ${data3}`, 'utf-8', err =>{
+          console.log('Your File has been written!');
+        })  
+    
+    
+    })
+
+   })
+});
+
+console.log("Will read file!");
